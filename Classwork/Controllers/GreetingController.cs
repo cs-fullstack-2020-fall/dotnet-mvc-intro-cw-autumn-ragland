52a169478063ef71b8c8289f0e6a1df30da71e15
autumn-ragland <raglanda998@gmail.com>
2020-08-18 16:21:34 -0500

    added comments

--- Classwork/Controllers/GreetingController.cs
+++ Classwork/Controllers/GreetingController.cs
@@ -4,10 +4,14 @@ namespace Classwork.Controllers
 {
     public class GreetingController : Controller
     {
+        // Endpoint to display simple message
+        // localhost:5001/Greeting/Greet
         public IActionResult Greet()
         {
             return Content("Well Hello There");
         }
+        // Endpoint with two query params that displays a simple message
+        // localhost:5001/Greetin/GreetUser?name=Autumn&visits=10
         public IActionResult GreetUser(string name, int visits)
         {
             return Content($"Well Hello There {name}! You have visited the site {visits} times!");
